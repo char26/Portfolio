9ffd838a3f0951efbc2485ff5090b3bccf03ba2a
Charlie Alders <charliealders26@gmail.com>
2024-06-17 01:45:57 -0700

    first version working!

--- public/js/my-console.js
+++ public/js/my-console.js
@@ -1,10 +1,13 @@
 class MyConsole {
     PROMPT = "charlie@portfolio:~$";
+    VALID_PROJECTS = ["gink"]; // TODO: add others
     mainContainer = document.getElementById("main-container");
     initialPrompt = document.getElementById("cursor");
     typeHere = document.getElementById("type-here");
     constructor() {
         this.trackedKeys = {};
+        this.createMessage("Welcome to my portfolio! Check out the commands below, or bring them back with -h or --help.");
+        this.printHelp();
     }
 
     onKeyDown(key) {
@@ -18,12 +21,12 @@ class MyConsole {
 
     handleKeyPress(key) {
         if (this.trackedKeys["Control"] && this.trackedKeys["c"]) {
-            nextLine();
+            this.nextLine();
         }
         if (key == "Enter") {
             const parsed = this.parseCommands(this.typeHere.textContent);
-            this.handleCommands(parsed);
             this.nextLine();
+            this.handleCommands(parsed);
         }
         else if (key == "Backspace") {
             if (this.typeHere.textContent.length > 0)
@@ -52,25 +55,86 @@ class MyConsole {
     }
 
     parseCommands(content) {
-        // TODO: super lazy, just want to get the concept up
-        return content.split(" ");
+        let [func, ...args] = (" " + content).split(' ');
+        args = Object.fromEntries(args.map(arg => arg.split('=')));
+        return args;
     }
 
     handleCommands(parsedCommands) {
         // TODO: super lazy, just want to get the concept up
-        if (!parsedCommands) return;
-        else if (parsedCommands.includes("-h") || parsedCommands.includes("--help")) {
-            const div = document.createElement('div');
-            div.setAttribute('class', 'prompt-cmd');
-            const output = document.createElement('p');
-            output.textContent = `
-            Lots of help
-
-
-            Stuff goes here
-            `;
-            div.appendChild(output);
-            this.mainContainer.insertBefore(div, this.initialPrompt);
+        const keys = Object.keys(parsedCommands);
+        if (keys.length > 1) {
+            this.createMessage("ERROR: too many arguments.");
+            return;
         }
+        if (!parsedCommands) return;
+        else if (keys.includes("-h") || keys.includes("--help")) {
+            this.printHelp();
+        }
+        else if (parsedCommands["projects"]) {
+            if (parsedCommands["projects"] == "all") {
+                for (const projectName of this.VALID_PROJECTS) {
+                    this.printProject(projectName);
+                }
+            }
+            else if (!this.VALID_PROJECTS.includes(parsedCommands["projects"])) {
+                this.createMessage("ERROR: not a valid project.");
+            }
+            this.printProject(parsedCommands["projects"]);
+        }
+        else if (keys.includes("github")) {
+            window.open("https://github.com/char26");
+        }
+        else if (keys.includes("linked-in")) {
+            window.open("https://www.linkedin.com/in/charles-alders-842bbb226/");
+        }
+        else if (keys.includes("clear")) {
+            this.clear();
+        }
+        else if (keys.includes("bio")) {
+            this.createMessage("TODO: bio");
+        }
+        else {
+            this.createMessage("ERROR: unknown command.");
+        }
+    }
+
+    printHelp() {
+        this.createMessage(`
+            commands:
+            bio <-------------------------------------------> get to know me!
+            github <--------------------------------------> opens a new tab to my GitHub profile.
+            linked-in <-----------------------------------> opens a new tab to my LinkedIn profile.
+            projects=all <-------------------------------> list all projects and their descriptions
+            projects=x [options: gink] <----------> list the description of a specific project
+            clear <-----------------------------------------> clear the console
+            `, true);
+    }
+
+    printProject(projectName) {
+        switch (projectName) {
+            case "gink":
+                this.createMessage("Gink: This is a project and i did it");
+                break;
+        }
+    }
+
+    createMessage(text, isHtml = false) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'prompt-cmd');
+        const output = document.createElement('p');
+        isHtml ? output.innerText = text : output.innerHTML = text;
+        div.appendChild(output);
+        this.mainContainer.insertBefore(div, this.initialPrompt);
+    }
+
+    clear() {
+        this.mainContainer.innerHTML = `
+        <div id="cursor" class="prompt-cmd">
+            <p id="prompt">charlie@portfolio:~$</p>
+            <p id="type-here"></p>
+        </div>`;
+        this.initialPrompt = document.getElementById("cursor");
+        this.typeHere = document.getElementById("type-here");
     }
 }
